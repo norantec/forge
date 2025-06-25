aee04213799967d1750477486427c768fa534909
lenconda <i@lenconda.top>
2025-06-25 15:02:13 +0800

    fix: 🐛 remove winston

--- src/cli.ts
+++ src/cli.ts
@@ -1,25 +1,9 @@
 #!/usr/bin/env node
 
-import * as winston from 'winston';
 import { createForgeCommand } from './forge';
-
-const logger = winston.createLogger({
-    level: 'verbose',
-    format: winston.format.combine(
-        winston.format.timestamp({
-            format: 'YYYY-MM-DD HH:mm:ss',
-        }),
-        winston.format.printf(
-            (info) =>
-                `${info.timestamp} - ${info.level}: ${info.message}` +
-                (info.splat !== undefined ? `${info.splat}` : ' '),
-        ),
-    ),
-    transports: [new winston.transports.Console()],
-});
 
 createForgeCommand({
     onLog: (level, message) => {
-        logger[level](message);
+        console.log(`[${new Date().toISOString()}] [${level}] ${message}`);
     },
 }).parse(process.argv);
