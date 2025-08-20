bfcba41b2d71af11eaa975e805956755a25ce946
lenconda <i@lenconda.top>
2025-08-20 14:45:58 +0800

    feat: 🎸 add fork ts plugin

--- src/forge.ts
+++ src/forge.ts
@@ -21,6 +21,7 @@ import * as originalFsPromises from 'fs/promises';
 import { VMUtil } from '@open-norantec/utilities/dist/vm-util.class';
 import { fork } from 'node:child_process';
 import { LogUtil } from '@open-norantec/utilities/dist/log-util.class';
+import ForkTsCheckerPlugin = require('fork-ts-checker-webpack-plugin');
 
 export type LogHandler = (level: Schema.LogLevel, message?: string) => void;
 
@@ -461,6 +462,7 @@ export class Forge {
                                 use: {
                                     loader: require.resolve('ts-loader'),
                                     options: {
+                                        transpileOnly: true,
                                         ...(StringUtil.isFalsyString(this.options?.tsCompiler)
                                             ? {}
                                             : { compiler: this.options.tsCompiler! }),
@@ -472,6 +474,9 @@ export class Forge {
                         ],
                     },
                     plugins: [
+                        new ForkTsCheckerPlugin({
+                            async: false,
+                        }),
                         new VirtualFilePlugin(volume),
                         new webpack.ProgressPlugin((percentage, message, ...args) => {
                             if (typeof this.inputOptions?.onProgress === 'function') {
